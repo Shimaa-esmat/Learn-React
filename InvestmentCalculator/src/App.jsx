c663bb0bafc7b67f26a641acea0b27c655005f40
Shimaa-esmat~ <shymaaesam01@gmail.com>
2024-07-21 02:46:52 +0300

    old one cannot handle multi input states

--- InvestmentCalculator/src/App.jsx
+++ InvestmentCalculator/src/App.jsx
@@ -1,28 +1,29 @@
 import Header from "./components/Header"
-import UserInput from "./components/UserInput"
+import path from './assets/investment-calculator-logo.png'
+// import UserInput from "./components/UserInput"
 import Result from "./components/Result.jsx"
 
 import { useState } from "react"
 
 
-  const path = "../src/assets/investment-calculator-logo.png"
   const headLine = `Investment Calculator`
   const InputContainer = "form"
-export default function App(){
-const[inputValues, setValues] = useState({})
 
+
+
+
+export default function App(){
 
   return (
     <>
     <Header path={path} headLine={headLine}/>
-    <InputContainer className="input-container">
-      <UserInput inputName="initial investment"/>
-      <UserInput inputName="annual investment"/>
-      <UserInput inputName="expected return"/>
-      <UserInput inputName="duration"/>
+    <InputContainer className="input-container" >
+      {/* <UserInput inputName="initial investment" initValue='10' /> */}
+      {/* <UserInput inputName="annual investment" initValue='10'  /> */}
+      {/* <UserInput inputName="expected return" initValue='10' /> */}
+      {/* <UserInput inputName="duration" initValue='10' /> */}
     </InputContainer>
     <Result/>
-
     </>
   )
 }
--- InvestmentCalculator/src/components/UserInput.jsx
+++ InvestmentCalculator/src/components/UserInput.jsx
@@ -1,19 +1,38 @@
 import { useState } from "react"
 
 
-export default function UserInput({inputName, initvalue}){
-    const [value, setValuea] = useState(initvalue)
-    function handleInputChange(e){
-        setValuea(e.target.value)
+export default function UserInput({inputName, initValue}){
+    const [value, setValuea] = useState({
+        'duration': initValue,
+        
+    })
+    if(inputName == 'duration'){
+        console.log('yesssssss')
     }
-    let idValue = inputName.replace(" ","-")
+    // console.log(value)
+    let idValue = inputName.replace(" ","")
+
+    
+    function handleInputChange(e){
+        // setValuea(e.target.value)
+        // field = {
+        //     [inputName]: e.target.value
+        // }
+        // console.log(field)
+
+        // return field
+    }
+
+    let field = {
+        "duration": value
+    }
+    console.log(field.duration)
+
     return(
     <div className="input-field">
         <label for={idValue}>{inputName}</label>    
-        <input type="number" id={idValue} onChange={handleInputChange} required/>
-        {console.log(value)}
-
+        <input type="number" id={idValue} value={value}
+        onChange={handleInputChange} required/>
     </div>
     )
 }
-
